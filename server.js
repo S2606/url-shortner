5e5b3c48a83ae09aed9fdfbe069ceb66a252e173
Shaggy2606 <shagun.khemka60@gmail.com>
2017-01-08 19:22:16 +0530

    make

--- server.js
+++ server.js
@@ -79,6 +79,6 @@ app.get('/:encoded_id', function(req, res){
     });
 });
 
-var server = app.listen(3000, function(){
+var server = app.listen(process.env.PORT || 3000, function(){
     console.log('Server listening on port 3000');
 });
